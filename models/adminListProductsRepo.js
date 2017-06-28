f2a949d921f07b35c4f397c6495ac11ae44dfe46
nth193 <nth193@atlassian.com>
2017-06-28 13:23:52 +0700

    updata products

--- models/adminListProductsRepo.js
+++ models/adminListProductsRepo.js
@@ -3,6 +3,6 @@ var mustache = require('mustache'),
 	db = require('../fn/db');
 
 exports.listuserpact = function(){
-	var sql = 'select pdg.maphien, sp.tensp, lsp.tenloai , pdg.giakhoidiem, pdg.giahientai, ha.urlhinhanh, sp.nguoidang, ltt.tentinhtrang from sanpham sp, loaisp lsp, phiendaugia pdg, hinhanh ha, loaitinhtrang ltt where sp.masp = pdg.sanpham and sp.masp = ha.masp and sp.loai = lsp.maloai and pdg.tinhtrang = ltt.maloai';
+	var sql = 'select pdg.maphien, sp.tensp, lsp.tenloai , pdg.giakhoidiem, pdg.giahientai, pdg.thgianbd, sp.nguoidang from sanpham sp, loaisp lsp, phiendaugia pdg where sp.masp = pdg.sanpham and sp.loai = lsp.maloai order by pdg.thgianbd asc';
 	return db.load(sql);
 }
