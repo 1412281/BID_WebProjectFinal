848a3aa7910b73853cd020e8518be960e90d139a
nth193 <nth193@atlassian.com>
2017-06-28 16:43:36 +0700

    backup

--- models/adminListProductRepo.js
+++ models/adminListProductRepo.js
@@ -6,3 +6,16 @@ exports.listproductpact = function(){
 	var sql = 'select pdg.maphien, sp.tensp, lsp.tenloai , pdg.giakhoidiem, pdg.giahientai, pdg.thgianbd, sp.nguoidang from sanpham sp, loaisp lsp, phiendaugia pdg where sp.masp = pdg.sanpham and sp.loai = lsp.maloai order by pdg.thgianbd asc';
 	return db.load(sql);
 }
+
+exports.updatestatus = function(id) {
+	var entity = {
+        maloai: id,
+        trangthai: 'ok'
+    };
+    var sql = mustache.render(
+        'update phiendaugia set tinhtrang = {{trangthai}} where maloai = {{maloai}}',
+        entity
+    );
+
+    return db.update(sql);
+}
