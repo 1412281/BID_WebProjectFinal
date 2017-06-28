e50ccf99a774529aaa675558dd85fea17286f6e7
nth193 <nth193@atlassian.com>
2017-06-28 11:04:29 +0700

    remove atribute ngaytao in users

--- models/adminListUserRepo.js
+++ models/adminListUserRepo.js
@@ -3,6 +3,6 @@ var mustache = require('mustache'),
 	db = require('../fn/db');
 
 exports.listuserpact = function(){
-	var sql = 'select u.tenuser, u.email, u.ngaytao, lqh.tenloai from users u, loaiuser lu, loaiquyenhan lqh where lqh.maloai = u.quyenhan and lu.maloai = u.loaiuser and lu.maloai = 2';
+	var sql = 'select u.tenuser, u.email, lqh.tenloai from users u, loaiuser lu, loaiquyenhan lqh where lqh.maloai = u.quyenhan and lu.maloai = u.loaiuser and lu.maloai = 2';
 	return db.load(sql);
 }
